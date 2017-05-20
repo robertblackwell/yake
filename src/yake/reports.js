a036c017ab15b3b6b74bd31eeba0e6a73fd6d1a8
ROBERT <rob@whiteacorn.com>
2017-05-20 10:15:11 +0200

    first

--- src/yake/reports.js
+++ src/yake/reports.js
@@ -1,4 +1,5 @@
 const TC = require('./task_collection.js');
+const chalk = require('chalk');
 
 exports.printTaskList = printTaskList;
 function printTaskList(collection)
@@ -7,12 +8,16 @@ function printTaskList(collection)
 	names.forEach((el, ix, ar) =>
 	{
 		const task = collection.getByName(el);
-		console.log('task : ${el}');
+		console.log(`task : ${el}`);
 		printTask(task);
 	})
 }
 
 function printTask(task)
 {
-	console.log(`printTask : ${task.name()}`);
+	const name = task.name();
+	const description = task.description();
+	const prereqs = task.prerequisites().join();
+	const line = `${chalk.reset.bold(name)} \t ${chalk.reset.cyan(description) \t [${prereqs}] }`;
+	console.log(line);
 }
