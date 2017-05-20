2a73533d9da91e885071afddc73e7bcf9a788910
ROBERT <rob@whiteacorn.com>
2017-05-20 09:56:04 +0200

    first

--- src/yake/error.js
+++ src/yake/error.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk');
 const process = require('process');
 
-exports.YakeError = {}
-exports.YakeError.raiseError = raiseError;
+// exports.YakeError = {}
+// exports.YakeError.raiseError = raiseError;
+exports.raiseError = raiseError;
 function raiseError(msgString, returnCode = -1)
 {
 	console.log(chalk.red('ERROR: ') + chalk.green(msgString));
